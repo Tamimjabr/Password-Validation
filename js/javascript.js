a4560e63eba1e1fdd59049a79db984bfe2d67850
T.J <Tamimjabr@github.com>
2020-06-20 21:03:41 +0200

    adding the main files

---
+++ js/javascript.js
@@ -0,0 +1,48 @@
+/*Displaying / Hiding the conditions */
+$("#password").focus(function(){
+    $("#warnings").css("display", "block");
+})
+$("#password").blur(function(){
+    $("#warnings").css("display", "none");
+})
+/*Testing the password everytime we change it*/
+$("#password").keyup(function(){
+    var myPassword= $("#password").val();
+    /*testing for small letters*/
+    if(/[a-z]/g.test(myPassword)){
+        $("#letter").removeClass("invalid");
+        $("#letter").addClass("valid");
+    }
+    else{
+         $("#letter").removeClass("valid");
+         $("#letter").addClass("invalid");
+    }
+    /*testing for capital letters*/
+    if(/[A-Z]/g.test(myPassword)){
+        $("#capital").removeClass("invalid");
+        $("#capital").addClass("valid");
+    }
+    else{
+        $("#capital").removeClass("valid");
+        $("#capital").addClass("invalid");
+    }
+    /*testing for small numbers*/
+    if(/[0-9]/g.test(myPassword)){
+        $("#number").removeClass("invalid");
+        $("#number").addClass("valid");
+    }
+    else{
+        $("#number").removeClass("valid");
+        $("#number").addClass("invalid");
+    }
+    /*testing for the length*/
+    if(myPassword.length>=8){
+        $("#length").removeClass("invalid");
+        $("#length").addClass("valid");
+    }
+    else{
+        $("#length").removeClass("valid");
+        $("#length").addClass("invalid");
+    }
+})
+
